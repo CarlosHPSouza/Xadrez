a4e9174a6d8363a3116fcf67ef42cb65b09c05f6
Carlos <carlos.chps@outlook.com>
2020-06-17 17:32:48 -0300

    Testes de movimentacao

--- xadrez/Program.cs
+++ xadrez/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using tabuleiro;
@@ -14,14 +15,22 @@ namespace xadrez
                                    
             try
             {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+                PartidaDeXadrez partida = new PartidaDeXadrez();
+                
+                while (!partida.terminada)
+                {
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
-
-                Tela.imprimirTabuleiro(tab);
+                    partida.excutaMovimento(origem, destino);
+                }
+                
                 Console.ReadKey();
             }
             catch(TabuleiroException e)
