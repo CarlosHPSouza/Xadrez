b12dbeeeed826fee311ce47229bf2fd36f52464f
Carlos <carlos.chps@outlook.com>
2020-06-17 15:12:52 -0300

    Implementando tabuleiro

--- xadrez/Program.cs
+++ xadrez/Program.cs
@@ -11,9 +11,7 @@ namespace xadrez
     {
         static void Main(string[] args)
         {
-            Posicao p;
-            p = new Posicao(3, 4);
-            Console.WriteLine("Posição: " + p);
+            Tabuleiro tab = new Tabuleiro(8, 8);
             Console.ReadKey();
         }
     }
